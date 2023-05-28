11cce5ab69a8f84ba66c793581ef7773e7b43ace
jay lee <91591854+erie0210@users.noreply.github.com>
2023-05-28 18:38:19 +0900

    Revert "[T 24] flyway 추가"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.0"
+	id("org.springframework.boot") version "3.0.6"
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
@@ -19,7 +19,6 @@ extra["springCloudVersion"] = "2022.0.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -28,18 +27,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	implementation("io.asyncer:r2dbc-mysql:1.0.1")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	runtimeOnly("io.r2dbc:r2dbc-h2")
-	runtimeOnly("com.h2database:h2")
-
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
-	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
-	testImplementation("org.flywaydb:flyway-core")
 }
 
 dependencyManagement {
--- src/main/kotlin/com/karrot42/account/repository/AccountRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.karrot42.account.repository
-
-import com.karrot42.account.domain.Account
-import org.springframework.data.r2dbc.repository.R2dbcRepository
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-
-interface AccountRepository : ReactiveCrudRepository<Account, Long>
