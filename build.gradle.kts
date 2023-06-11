ad4e0a50fe93aaf9a8dd7596680a8b7c0a1f3605
Kim Myeongil <mikim961103@gmail.com>
2023-06-12 00:41:00 +0900

    feat: session persistence 를 위해 redis 사용

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2022.0.2"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation("org.springframework.session:spring-session-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
